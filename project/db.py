486b6507bcf47e81aeca85a236a686860ace1ec9
Артём <tyomych234908@gmail.com>
2024-02-05 23:16:09 +0400

    v.1

---
+++ project/db.py
@@ -0,0 +1,10 @@
+import sqlite3
+
+
+class BotDB:
+    def __init__(self, db_file) -> None:
+        self.database = sqlite3.connect(db_file)
+        self.cursor = self.database.cursor()
+
+    def close(self):
+        self.database.close()
