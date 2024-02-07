fde6326cf3e4876819d71134f4d81740d226e51f
Артём <tyomych234908@gmail.com>
2024-02-07 23:58:46 +0400

    v.1.1

--- project/main.py
+++ project/main.py
@@ -4,33 +4,12 @@ from aiogram import Bot, Dispatcher
 from aiogram.filters import Command, CommandObject
 from aiogram.types import Message
 
-import json
-import random
-
-from db import BotDB
+from other import BotDB, Functions
 from config import settings
 
-bot_db = BotDB("project.db")
+bot_db = BotDB("files/project.db")
 bot = Bot(token=settings['token'])
 dp = Dispatcher()
-
-def get_new_exercise() -> tuple:
-    """
-        Возвращает кортеж, состоящий из списка слов и последовательности цифр, являющейся номерами верно написанных слов
-    """
-    with open("pr.json", "r", encoding="utf-8") as file:
-        data = list(json.load(file).items())
-        amount_true_answers = random.randint(1,4)
-        true_answers = [random.choice(data)[0] for i in range(amount_true_answers)]  
-        false_answers = [random.choice(random.choice(data)[1]) for i in range(5-amount_true_answers)]  
-        result = true_answers.copy() + false_answers.copy()
-        random.shuffle(result)
-        
-        answer = ""
-        for i in range(len(result)):
-            if result[i] in true_answers:
-                answer += str(i+1)
-        return (result, answer)
 
 @dp.message(Command("start"))
 async def start(message: Message):
