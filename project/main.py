e727041b6769b97e9c8ab9ad33a75662def59f01
Артём <tyomych234908@gmail.com>
2024-02-19 22:53:33 +0400

    v.1.2

--- project/main.py
+++ project/main.py
@@ -1,10 +1,14 @@
 import asyncio
-import sqlite3
-from aiogram import Bot, Dispatcher
+from aiogram import Bot, Dispatcher, types, F
 from aiogram.filters import Command, CommandObject
+from aiogram.utils.keyboard import InlineKeyboardBuilder
 from aiogram.types import Message
 
-from other import BotDB, Functions
+import sqlite3
+from random import shuffle
+
+from BotDB import BotDB
+from other_funtions import Functions
 from config import settings
 
 bot_db = BotDB("files/project.db")
@@ -12,8 +16,112 @@ bot = Bot(token=settings['token'])
 dp = Dispatcher()
 
 @dp.message(Command("start"))
-async def start(message: Message):
-    await message.answer("Hello!")
+async def cmd_start(message: types.Message):
+    builder = InlineKeyboardBuilder()
+    builder.add(types.InlineKeyboardButton(
+        text="Главное меню",
+        callback_data="menu")
+    )
+    await message.answer("Выберите действие", reply_markup=builder.as_markup())
+
+@dp.callback_query(F.data == "menu")
+async def menu(callback: types.CallbackQuery):
+    await callback.message.delete()
+    builder = InlineKeyboardBuilder()
+    builder.add(types.InlineKeyboardButton(
+        text="Профиль и статистика",
+        callback_data="profile")
+    )
+    builder.add(types.InlineKeyboardButton(
+        text="Тренировка",
+        callback_data="train")
+    )
+    await callback.message.answer("Главное меню", reply_markup=builder.as_markup())
+
+@dp.callback_query(F.data == "profile")
+async def profile(callback: types.CallbackQuery):
+    await callback.message.delete()
+    builder = InlineKeyboardBuilder()
+    builder.add(types.InlineKeyboardButton(
+        text="Главное меню",
+        callback_data="menu")
+    )
+    await callback.message.answer("В разработке", reply_markup=builder.as_markup())
+
+
+@dp.callback_query(F.data == "train")
+async def menu(callback: types.CallbackQuery):
+    await callback.message.delete()
+    builder = InlineKeyboardBuilder()
+    builder.add(types.InlineKeyboardButton(
+        text="Задание 4",
+        callback_data="ex_4")
+    )
+    builder.add(types.InlineKeyboardButton(
+        text="Главное меню",
+        callback_data="menu")
+    )
+    await callback.message.answer(
+        "Выберите задание, которое хотели бы прорешать",
+        reply_markup=builder.as_markup())
+    
+@dp.callback_query(F.data == "ex_4")
+async def send_new_ex_4(callback: types.CallbackQuery):
+    await callback.message.delete()
+    data = Functions.get_the_fourth_task()
+    answers = [data[1]]
+    answers.extend(data[2])
+    shuffle(answers)
+
+    builder = InlineKeyboardBuilder()
+    for i in answers:
+        if i == data[1]:
+            builder.add(types.InlineKeyboardButton(
+                text=f"{i}",
+                callback_data="true_answer_ex_4")
+            )
+        else:
+            builder.add(types.InlineKeyboardButton(
+                text=f"{i}",
+                callback_data="false_answer_ex_4")
+            )
+
+    await callback.message.answer(
+        f"""Укажите вариант ответа, в котором во всех словах верно выделена буква, обозначающая ударный гласный звук.
+        [1] {data[0][0]}
+        [2] {data[0][1]}
+        [3] {data[0][2]}
+        [4] {data[0][3]}
+        [5] {data[0][4]}""", 
+        reply_markup=builder.as_markup())
+
+@dp.callback_query(F.data == "true_answer_ex_4")
+async def true_answer_ex_4(callback: types.CallbackQuery):
+    await callback.message.delete()
+    builder = InlineKeyboardBuilder()
+    builder.add(types.InlineKeyboardButton(
+                text=f"Продолжить",
+                callback_data="ex_4")
+            )
+    builder.add(types.InlineKeyboardButton(
+        text="Главное меню",
+        callback_data="menu")
+    )
+    await callback.message.answer("Ты ахуеть как прав братишка", reply_markup=builder.as_markup())
+
+@dp.callback_query(F.data == "false_answer_ex_4")
+async def false_answer_ex_4(callback: types.CallbackQuery):
+    await callback.message.delete()
+    builder = InlineKeyboardBuilder()
+    builder.add(types.InlineKeyboardButton(
+                text=f"Продолжить",
+                callback_data="ex_4")
+            )
+    builder.add(types.InlineKeyboardButton(
+        text="Главное меню",
+        callback_data="menu")
+    )
+    await callback.message.answer("Боже чел как мне за тебя стыдно ты больше не репер", reply_markup=builder.as_markup())
 
 async def main():
     await bot.delete_webhook(drop_pending_updates=True)
---
+++ project/other_funtions.py
@@ -0,0 +1,42 @@
+import json
+import random
+
+class Functions:
+    def get_the_fourth_task() -> tuple:
+        with open("files/ex4.json", "r", encoding="utf-8") as file:
+            data = list(json.load(file).items())
+
+            amount_true_answers = random.randint(2,4)
+            true_answers = [random.choice(data)[0] for i in range(amount_true_answers)]  
+            false_answers = [random.choice(random.choice(data)[1]) for i in range(5-amount_true_answers)]  
+            result = true_answers.copy() + false_answers.copy()
+            random.shuffle(result)
+            #result - список слов, в которых выделено ударение
+
+            answer = ""
+            for i in range(len(result)):
+                if result[i] in true_answers:
+                    answer += str(i+1)
+            #answer - номера верно выделенных в словах ударений 
+
+            list_with_wrong_anwers = []
+            while len(list_with_wrong_anwers) < 3:
+                numbers = "12345"
+                temp = ""
+                for i in numbers:
+                    temp_2 = random.randint(0,2)
+                    if temp_2:
+                        temp += i
+                if (temp != "") and (temp != result) and (temp not in list_with_wrong_anwers):
+                    list_with_wrong_anwers.append(temp)
+            #list_with_wrong_anwers - список со случайными числами, которые состоят из цифр от 1 до 5. 
+
+            return (result, answer, list_with_wrong_anwers)
+        
+    def get_the_fifth_task():
+        with open("files/ex5.json", "r", encoding="utf-8") as file:
+            data = list(json.load(file).items())
+            for i in data:
+                print(i)
+
+Functions.get_the_fifth_task()
