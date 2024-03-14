9db6cca29bb28bcc849adb52bff7518081866c41
Артём <tyomych234908@gmail.com>
2024-03-14 13:26:01 +0400

    1.8

--- project/config.py
+++ project/config.py
@@ -1,5 +1,5 @@
 settings = {
-    "token": "",
+    "token": "6726807918:AAHnQZTR6CN2fhbVzDNfGQ0fj35iOLF0GXg",
     "db_path": "files/project.db",
     "ex4_path":"files/ex4.json",
     "ex5_path":"files/ex5.json",
